db71477e476ccae948b6434be461ab841edf8d73
MrSplinter1337 <149711411+MrSplinter1337@users.noreply.github.com>
2024-09-30 15:44:28 +0600

    Update app.py

--- app.py
+++ app.py
@@ -114,4 +114,5 @@ def generate_captcha():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", 5000))  # Получаем порт из переменной окружения или используем 5000 по умолчанию
+    app.run(host="0.0.0.0", port=port)
